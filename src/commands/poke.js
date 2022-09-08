8da9e75c2a56f8130fc2e3e016162bca2b221dc1
Jason Kha <jasonkha63@yahoo.com>
2022-09-08 17:48:36 -0500

    Removed log

--- src/commands/poke.js
+++ src/commands/poke.js
@@ -8,7 +8,6 @@ export default {
             option.setName('user').setDescription('User to poke').setRequired(true)
         ),
     async execute(interaction) {
-        console.log(interaction.member);
         await interaction.reply(
             `${'<@' + interaction.member.user.id + '>'} poked ${
                 '<@' + interaction.options.getUser('user').id + '>'
