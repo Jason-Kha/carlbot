ef20f52b1b2ee91243d8b56d0071744af315937d
Jason Kha <jasonkha63@yahoo.com>
2022-09-08 15:28:26 -0500

    Added presence

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,7 +1,15 @@
+import { ActivityType } from 'discord.js';
+
 export default {
     name: 'ready',
     once: true,
     execute(client) {
         console.log(`Ready! Logged in as ${client.user.tag}`);
+
+        // set presence
+        client.user.setPresence({
+            activities: [{ name: 'eating hands', type: ActivityType.Competing }],
+            status: 'online'
+        });
     }
 };
