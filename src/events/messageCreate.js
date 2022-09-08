478f98108e877364aaa2f3dbe72213f201b07402
Jason Kha <jasonkha63@yahoo.com>
2022-09-08 16:47:16 -0500

    Message event gamer words

---
+++ src/events/messageCreate.js
@@ -0,0 +1,11 @@
+export default {
+    name: 'messageCreate',
+    async execute(message) {
+        const content = message.content;
+
+        // gamer moment
+        if (content.toLowerCase().includes('gamer') && !message.author.bot) {
+            await message.reply('gamer');
+        }
+    }
+};
