d21584831cc983b05cf424e8512e77f5a772fa4b
Jason Kha <jasonkha63@yahoo.com>
2022-09-09 13:33:09 -0500

    Gamer letter

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,13 +1,24 @@
 export default {
     name: 'messageCreate',
     async execute(message) {
-        if (message.content.bot) return;
+        if (message.author.bot) return;
 
         const content = message.content;
 
         // gamer moment
         if (content.toLowerCase().includes('gamer')) {
             await message.reply('gamer');
+            return;
+        }
+
+        // letter reactions
+        if (
+            content.toLowerCase().split(' ').includes('g') ||
+            content.toLowerCase().split(' ').includes('ñ') ||
+            content.toLowerCase().split(' ').includes('h')
+        ) {
+            await message.react('🇬');
+            return;
         }
         return;
     }
