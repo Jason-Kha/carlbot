ef2a3fcce3eef1ef9b5f57e1da27ff7b802a6510
Jason Kha <jasonkha63@yahoo.com>
2022-09-09 10:28:08 -0500

    Updated logic

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,10 +1,12 @@
 export default {
     name: 'messageCreate',
     async execute(message) {
+        if (message.content.bot) return;
+
         const content = message.content;
 
         // gamer moment
-        if (content.toLowerCase().includes('gamer') && !message.author.bot) {
+        if (content.toLowerCase().includes('gamer')) {
             await message.reply('gamer');
         }
         return;
