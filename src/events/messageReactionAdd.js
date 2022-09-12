986eef2d21cc60f5a782866e03e089d23d237bca
Jason Kha <jasonkha63@yahoo.com>
2022-09-11 21:33:44 -0500

    Update reactions

--- src/events/messageReactionAdd.js
+++ src/events/messageReactionAdd.js
@@ -1,8 +1,15 @@
 export default {
     name: 'messageReactionAdd',
-    async execute(messageReaction) {
-        console.log(messageReaction.users);
+    async execute(messageReaction, user) {
+        if (user.bot) return;
+        // const id = '257303326184570880';
+        const id = '173490982023593984';
+        console.log(messageReaction);
 
+        // if id matches user
+        if (user.id === id) {
+            messageReaction.message.react('😎');
+        }
         return;
     }
 };
